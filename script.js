15075de1273d11bc2795874806fba03d550fbdec
isamello2008 <144174288+isamello2008@users.noreply.github.com>
2025-04-01 10:47:35 +0000

    java contraste

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = this.documentElementById ('alterna-contraste')
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
